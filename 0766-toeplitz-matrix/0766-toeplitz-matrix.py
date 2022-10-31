ef020906ac8fcb12edfe16c3d58817c9c38eddeb
Palla Venkata Sai <72677771+venkatsaipalla@users.noreply.github.com>
2022-10-31 22:36:19 +0530

    Time: 191 ms (45.60%), Space: 13.9 MB (78.65%) - LeetHub

---
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -0,0 +1,36 @@
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        row=len(matrix)
+        col=len(matrix[0])
+        i=0
+        j=1
+        while i<row:
+            r=i
+            c=0
+            flag=0
+            while r+1<row and c+1<col:
+                if matrix[r][c]!=matrix[r+1][c+1]:
+                    flag=1
+                r=r+1
+                c=c+1
+            if flag==1:
+                return False
+            i=i+1
+        while j<col:
+            r=0
+            c=j
+            flag=0
+            while r+1<row and c+1<col:
+                if matrix[r][c]!=matrix[r+1][c+1]:
+                    flag=1
+                r=r+1
+                c=c+1    
+            if flag==1:
+                return False 
+            j=j+1
+        return True    
+            
+                
+        
+        
+        
