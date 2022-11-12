566be011239f4378d3a23de789f4d1d0173c8aa6
Palla Venkata Sai <72677771+venkatsaipalla@users.noreply.github.com>
2022-11-12 21:12:45 +0530

    Time: 71 ms (11.71%), Space: 13.8 MB (66.16%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,19 +1,12 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         length=len(nums)
-        if length == 1:
+        if length==1:
             return nums[0]
-        
-        if length == 2:
-            return max(nums[0], nums[1])
-        
-        robFirst = nums[0]
-        robSecond = max(nums[0], nums[1])
-        
-        for i in range(2, length):
-            current = max(robSecond, robFirst + nums[i])
-            
-            robFirst = robSecond
-            robSecond = current
-            
-        return robSecond
+        if length==2:
+            return max(nums[0],nums[1])
+        else:
+            nums[1]=max(nums[1],nums[0])
+            for i in range(2,len(nums)):
+                nums[i]=max(nums[i]+nums[i-2],nums[i-1])
+            return nums[-1]    
