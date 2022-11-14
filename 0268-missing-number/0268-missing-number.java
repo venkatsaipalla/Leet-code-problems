3487d4f67b433c800ce6a250a51c5528f682f1e5
Palla Venkata Sai <72677771+venkatsaipalla@users.noreply.github.com>
2022-11-14 23:18:42 +0530

    Time: 10 ms (16.51%), Space: 51.1 MB (42.97%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int missingNumber(int[] nums) { //binary search
+    Arrays.sort(nums);
+    int left = 0, right = nums.length, mid= (left + right)/2;
+    while(left<right){
+        mid = (left + right)/2;
+        if(nums[mid]>mid) right = mid;
+        else left = mid+1;
+    }
+    return left;
+}
+}
