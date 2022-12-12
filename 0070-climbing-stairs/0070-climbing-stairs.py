939d767c898bc15d61d5cf9619fda54e3c67e540
Palla Venkata Sai <72677771+venkatsaipalla@users.noreply.github.com>
2022-12-12 10:19:14 +0530

    Time: 49 ms (65.03%), Space: 13.8 MB (58.08%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -5,11 +5,3 @@ class Solution:
             return self.d[n]
         self.d[n]= self.climbStairs(n-1)+self.climbStairs(n-2)
         return self.d[n]
-        '''
-        class Solution:
-    d={1:1,2:2}
-    def climbStairs(self, n: int) -> int:
-        if self.d.get(n,0):return self.d.get(n)
-        self.d[n]=self.climbStairs(n-1)+self.climbStairs(n-2)
-        return self.d[n]
-        '''
