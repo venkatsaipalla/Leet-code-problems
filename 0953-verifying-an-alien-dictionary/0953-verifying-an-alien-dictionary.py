b28defb5d809d04e47f6c6f7b56cbeb482c86c32
Palla Venkata Sai <72677771+venkatsaipalla@users.noreply.github.com>
2023-02-03 22:55:58 +0530

    Time: 46 ms (38.06%), Space: 13.9 MB (77.53%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -1,5 +1,6 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
+        #v
         l1 = {c:i for i,c in enumerate(order)}
         l2 = [[l1[i] for i in word] for word in words]
         return l2 == sorted(l2)
