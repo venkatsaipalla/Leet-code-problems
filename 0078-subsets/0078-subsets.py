12f16827222e97a4539159bd9633ae17d1b3b09b
Palla Venkata Sai <72677771+venkatsaipalla@users.noreply.github.com>
2023-03-17 21:03:24 +0530

    Time: 34 ms (75.25%), Space: 14.1 MB (77.85%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def subsets(self, nums):
+        nums.sort()
+        result = [[]]
+        for num in nums:
+            result += [i + [num] for i in result]
+        return result
