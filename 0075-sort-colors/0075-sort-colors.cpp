4cd0fe75954350b60903d4862bcbdc3e352b825e
Palla Venkata Sai <72677771+venkatsaipalla@users.noreply.github.com>
2023-07-26 08:22:05 +0530

    Time: 7 ms (23.91%), Space: 8.3 MB (37.76%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -8,10 +8,10 @@ public:
         
     void sortColors(vector<int>& nums) {
        
-        for(int i=0;i<nums.size();++i){
-            for(int j=i+1;j<nums.size();++j){
-                if(nums[i]>nums[j]){
-                    swap(nums[i],nums[j]);
+        for(int i=0;i<nums.size()-1;++i){
+            for(int j=0;j<nums.size()-i-1;++j){
+                if(nums[j]>nums[j+1]){
+                    swap(nums[j],nums[j+1]);
                 }
             }
         }
