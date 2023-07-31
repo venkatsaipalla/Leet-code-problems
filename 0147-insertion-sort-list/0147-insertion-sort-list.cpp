6a81db667e89ee716c0503d882c8e6df47b427ad
Palla Venkata Sai <72677771+venkatsaipalla@users.noreply.github.com>
2023-07-31 23:36:16 +0530

    Time: 5 ms (100.00%), Space: 9.6 MB (55.70%) - LeetHub

--- 0147-insertion-sort-list/0147-insertion-sort-list.cpp
+++ 0147-insertion-sort-list/0147-insertion-sort-list.cpp
@@ -8,37 +8,55 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-#include <iostream>
-using namespace std;
+
 class Solution {
 public:
     ListNode* insertionSortList(ListNode* head) {
-        ListNode* start= new ListNode(0);
-        start=head;
-        vector<int>arr={};
-        int i,j,key,n;
-        while(start!=NULL){
-            arr.push_back(start->val);
-            start=start->next;
-        }
-        n=arr.size();
-        for(i=1;i<n;i++){
-            key=arr[i];
-            j=i-1;
-            while(j>=0 && arr[j]>key){
-                arr[j+1]=arr[j];
-                j--;
-            }
-            arr[j+1]=key;      
-        }
         ListNode* dummy = new ListNode(0);
-        dummy->val=arr[0];
-        head=dummy;
-        for(i=1;i<n;i++){
-            ListNode* temp = new ListNode(arr[i]);
-            dummy->next=temp;
-            dummy=dummy->next;
+        ListNode* curr =head;
+        ListNode* prev = dummy;
+        prev->next=head;
+        while(curr!=NULL){
+            if(curr->next!=NULL && curr->next->val < curr->val){
+                while(prev->next!=NULL && prev->next->val < curr->next->val){
+                    prev=prev->next;
+                }
+                ListNode* temp=prev->next;
+                prev->next=curr->next;
+                curr->next=curr->next->next;
+                prev->next->next=temp;
+                prev=dummy;
+            }else{
+                curr=curr->next;
+            }
         }
-        return head;
-    }
-};
+    
+        return dummy->next;
+          
+}};
+
+// class Solution {
+// public:
+//     ListNode* insertionSortList(ListNode* head) {
+//       ListNode* dummy = new ListNode(0);
+//       ListNode* curr = head;
+//       ListNode* prev = dummy;
+//       prev->next = head;
+//       while(curr!=NULL){
+//           if(curr->next!=NULL && curr->next->val<curr->val){
+//               while(prev->next!=NULL && prev->next->val<curr->next->val){
+//                   prev=prev->next;
+//               }
+//               ListNode* temp=prev->next;
+//               prev->next = curr->next;
+//               curr->next = curr->next->next;
+//               prev->next->next = temp;
+//               prev = dummy;
+//           }
+//           else{
+//               curr=curr->next;
+//           }
+//       }
+//     return dummy->next;
+//     }
+// };
