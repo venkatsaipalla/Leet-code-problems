3aa45e8e8512b90b935fcbd96307e0dbc8eaeb7f
Palla Venkata Sai <72677771+venkatsaipalla@users.noreply.github.com>
2023-08-01 09:50:12 +0530

    Time: 464 ms (70.29%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,5 +1,13 @@
 # Write your MySQL query statement below
 # select ifnull(null,salary) as SecondHighestSalary from Employee
 # order by salary DESC limit 1 offset 1;
-select max(salary) as SecondHighestSalary from Employee 
-where salary<(select max(salary) from Employee);
+
+# Approch-1
+# select max(salary) as SecondHighestSalary from Employee 
+# where salary<(select max(salary) from Employee);
+
+#Approch -2
+with temp (max_salary) as
+(select max(salary) from Employee) 
+select max(salary) as SecondHighestSalary
+from Employee,temp where Employee.salary<temp.max_salary;
