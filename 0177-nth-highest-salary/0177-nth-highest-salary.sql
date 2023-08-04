e80de374d16fb3125eab9ebc6b5e78e467a8a240
Palla Venkata Sai <72677771+venkatsaipalla@users.noreply.github.com>
2023-08-04 09:40:58 +0530

    Time: 749 ms (71.43%), Space: 0B (100.00%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -1,11 +1,23 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-SET N=N-1;
+# SET N=N-1;
   RETURN (
       # Write your MySQL query statement below.
       
-      select DISTINCT(salary) from Employee
-      order by salary DESC limit 1 offset N
+      # select DISTINCT(salary) from Employee
+      # order by salary DESC limit 1 offset N
+      
+      select distinct salary from (
+      select salary , DENSE_RANK() 
+          OVER (order by salary DESC) as rankedSalary
+          from Employee) as T
+      where rankedSalary = N
       
   );
 END
+
+-- Why use dense_rank() instead of rank ?
+/*
+    DENSE_RANK() -- ranks are consecutive
+    RANK() -- ranks are skipped
+*/
