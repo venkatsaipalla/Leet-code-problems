11396e73197f976267775b970e9e3098e9f3ee25
Palla Venkata Sai <72677771+venkatsaipalla@users.noreply.github.com>
2023-08-22 10:22:43 +0530

    Time: 1615 ms (38.33%), Space: 0B (100.00%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -4,7 +4,7 @@ select Department,Employee,Salary
 from (
 select d.name as Department,
 e.name as Employee, e.salary as Salary,
-RANK() over(partition by d.name order by e.salary DESC) as s_rank
+DENSE_RANK() over(partition by d.name order by e.salary DESC) as s_rank
 from Employee as e  join Department as d on e.departmentId=d.id
 )as t
 where t.s_rank=1
