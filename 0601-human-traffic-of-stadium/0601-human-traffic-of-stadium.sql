aa9a7ef175fe7a9b1dc04390034350cfd8e0dbd8
Palla Venkata Sai <72677771+venkatsaipalla@users.noreply.github.com>
2023-09-04 17:03:32 +0530

    Time: 731 ms (72.06%), Space: 0B (100.00%) - LeetHub

--- 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
+++ 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
@@ -1,8 +1,20 @@
-select distinct t1.*
-from stadium t1 join stadium t2 join stadium t3
-on (t1.id=t2.id-1 and t1.id=t3.id-2)
-or (t1.id=t2.id+1 and t1.id=t3.id-1)
-or (t1.id=t2.id+1 and t1.id=t3.id+2)
+# select distinct t1.*
+# from stadium t1 join stadium t2 join stadium t3
+# on (t1.id=t2.id-1 and t1.id=t3.id-2)
+# or (t1.id=t2.id+1 and t1.id=t3.id-1)
+# or (t1.id=t2.id+1 and t1.id=t3.id+2)
 
-where t1.people>=100 and t2.people>=100 and t3.people>=100
-order by visit_date
+# where t1.people>=100 and t2.people>=100 and t3.people>=100
+# order by visit_date
+
+with group_number as (select * ,
+row_number() over(order by visit_date) as rn,
+id-row_number() over(order by visit_date) as grp
+from 
+stadium 
+where people>=100)
+
+select id,visit_date,people from group_number
+where grp in 
+(select grp from group_number 
+group by grp having count(1)>=3)
