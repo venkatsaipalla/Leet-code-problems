09d4d1539a0e95170572276c9c068c82186068a7
Palla Venkata Sai <72677771+venkatsaipalla@users.noreply.github.com>
2023-09-06 09:20:47 +0530

    Time: 8 ms (91.03%), Space: 13.5 MB (35.84%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,25 +3,28 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m=matrix.size();
         int n=matrix[0].size();
+        
         int col0=1;
         for(int i=0;i<m;++i){
             for(int j=0;j<n;++j){
                 if(matrix[i][j]==0){
-                    //making row's first element as 0
+                    //making first element of each row as 0
                     matrix[i][0]=0;
-                    //making col's first element as 0
+                    
+                    //making first element of each colum as 0
                     if(j!=0)
                         matrix[0][j]=0;
-                     else
-                         col0=0;                  
+                    else{
+                        col0=0;
+                    }    
                 }
             }
         }
         
-        for(int i=1;i<m;++i){
-            for(int j=1;j<n;++j){
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
                 if(matrix[i][j]!=0){
-                    if(matrix[i][0]==0 or matrix[0][j]==0)
+                    if(matrix[0][j]==0 or matrix[i][0]==0)
                         matrix[i][j]=0;
                 }
             }
@@ -33,9 +36,11 @@ public:
             }
         }
         if(col0==0){
-             for(int i=0;i<m;i++){
+            for(int i=0;i<m;i++){
                 matrix[i][0]=0;
             }
         }
+            
+    
         
 }};
