f4b8e1c463885e5db5337f3aa025c45eb0d9fcf2
Palla Venkata Sai <72677771+venkatsaipalla@users.noreply.github.com>
2023-09-18 19:11:07 +0530

    Time: 15 ms (62.48%), Space: 8.9 MB (47.65%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -8,13 +8,10 @@ public:
         map<char,int>map;
         map.insert({s[0],0});
         int i=1;
-        cout<<"-----------"<<endl;
         int start=0;
         while(i<n){
-            
            if(map.find(s[i])==map.end()){
                map[s[i]]=i;
-               cout<<"map--i="<<i<<"  "<<map[s[i]]<<endl;
                count++;
            }
             else if(map[s[i]]<start){
@@ -24,18 +21,9 @@ public:
            else{
                start=map[s[i]]+1;
                count=i-map[s[i]];
-               // for(auto itr:map){
-               //     if(itr.second<map[s[i]]) map.erase(itr.first);
-               // }
-               //  for(auto itr:map){
-               //     cout<<itr.first;
-               // }
-               cout<<endl;
-               cout<<"duplicate s[i] = "<<s[i]<<" map->"<<map[s[i]]<<endl;
                map[s[i]]=i;
            } 
              maxCount=max(maxCount,count);
-            cout<<"\ni = "<<i<<" s[i] = "<<s[i]<<"  "<<"count = "<<count<<endl;
             i++;
         }
         return maxCount;
